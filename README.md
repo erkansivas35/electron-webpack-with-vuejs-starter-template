9eacb405a9be74da8142ac38d2d79044f8d8408f
erkansivas35 <erkan.sivas35@gmail.com>
2018-08-08 14:40:11 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Electron with webpack and Vue.js Starter Template
+
+Install [Node.js](https://nodejs.org/).
+
+Within this folder run the terminal command `npm install`
+
+Then run `npm start` to run the app.
---
+++ src/renderer/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="app">
+    <h1>{{name}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "Electron Starter App"
+    };
+  }
+};
+</script>
+
+<style>
+.app {
+  width: 100%;
+  height: calc(100vh - 20px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: calc(1.5vh + 1.5vw);
+}
+</style>
