513b90ecde6397fd2204826c26383033d3cc3a1d
erkansivas35 <erkan.sivas35@gmail.com>
2018-08-29 15:41:39 +0300

    the router left index.js.

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,21 +1,6 @@
 import Vue from 'vue'
-import Router from 'vue-router'
-
-Vue.use(Router)
-
 import App from './App.vue'
-import Home from './components/Home.vue'
-
-const router = new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: Home
-    }
-  ]
-})
-
+import router from './router'
 
 new Vue({
   el: '#app',
